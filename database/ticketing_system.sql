4dfaa7472ac6713ff5fa4516ccecdee917112935
DhanushP92922 <dhanushp3542@gmail.com>
2025-03-18 14:06:06 +0530

    Update ticketing_system.sql

--- database/ticketing_system.sql
+++ database/ticketing_system.sql
@@ -44,7 +44,7 @@ CREATE TABLE `admins` (
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`, `category`, `role`, `created_at`, `profile_picture`) VALUES
 (1, 'Super Admin', 'superadmin@example.com', '$2y$10$mAZ5zBMCKWvGCR4LDjhgb.Dbb.VSkX.lN6FqnABhccZ/XryBl/1lq', NULL, 'superadmin', '2025-02-26 07:55:15', NULL),
-(2, 'admin1', 'zzz@gmail.com', '$2y$12$t0GTZuvJ40vov7oaJt.AVeeSmeqtsQ28TyebMgAD3CC5AteDWGgA6', 'Technical Support', 'admin', '2025-03-01 09:28:54', 'uploads/profile_pictures/1740922555_705149ad4934fbda556f.jpg'),
+(2, 'admin1', 'zzz@gmail.com', '$2y$12$t0GTZuvJ40vov7oaJt.AVeeSmeqtsQ28TyebMgAD3CC5AteDWGgA6', 'Technical Support', 'admin', '2025-03-01 09:28:54',NULL),
 (3, 'yyy', 'yyy@gmail.com', '$2y$12$6FcVDeUj34GEUV.7BZapDuYWwKj3Fyz5ShV97qbzfa6a6CF9D28uq', 'Billing', 'admin', '2025-03-01 09:50:46', NULL),
 (4, 'www', 'www@gmail.com', '$2y$12$i4adAQrZiEyVkhVsz2bg0ux3JW438x2SSKhygABCh78LB8abB5cHa', 'General Inquiry', 'admin', '2025-03-01 09:51:17', NULL),
 (5, 'vvv', 'vvv@gmail.com', '$2y$12$p.Xf1GuVOV2NKqd1Uakn6eDExXv7LaQCRe1l24CxmpgF2jdSRsMkW', 'Account Issues', 'admin', '2025-03-01 09:51:43', NULL),
@@ -97,16 +97,6 @@ CREATE TABLE `messages` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`id`, `ticket_id`, `admin_id`, `user_id`, `sender`, `message`, `created_at`, `attachment`) VALUES
-(130, 'TICKET-8E5AF3E2', 2, 26, 'user', 'hi', '2025-03-18 05:36:21', NULL),
-(131, 'TICKET-8E5AF3E2', 2, NULL, 'admin', 'hello', '2025-03-18 05:38:45', '');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tickets`
 --
 
@@ -125,15 +115,6 @@ CREATE TABLE `tickets` (
   `ticket_id` varchar(50) DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tickets`
---
-
-INSERT INTO `tickets` (`id`, `user_id`, `email`, `title`, `description`, `category`, `status`, `urgency`, `assigned_admin_id`, `created_at`, `attachments`, `ticket_id`, `updated_at`) VALUES
-(106, 26, 'hello@gmail.com', 'hello', 'hello', 'Technical Support', 'Open', 'High', 2, '2025-03-18 05:35:49', '[\"uploads\\/tickets\\/1742276149_b1665aecf53e53acae4a.png\"]', 'TICKET-8E5AF3E2', '2025-03-18 05:44:10');
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `ticket_status_log`
@@ -160,35 +141,6 @@ CREATE TABLE `users` (
   `email` varchar(100) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `created_at`) VALUES
-(1, '111', '111@gmail.com', '2025-02-26 08:00:30'),
-(2, 'aaaa', 'aaaa@gmail.com', '2025-02-26 08:02:44'),
-(5, 'dhanush.p', 'dhanush3542@gmail.com', '2025-03-01 05:16:56'),
-(6, 'bbbb', 'bbbb@gmail.com', '2025-03-01 05:21:14'),
-(7, 'ppp', 'ppp@gmail.com', '2025-03-02 11:51:08'),
-(9, NULL, 'ttt@gmail.com', '2025-03-08 08:17:13'),
-(10, NULL, 'sss@gmail.com', '2025-03-09 05:54:16'),
-(11, NULL, 'fff@gmail.com', '2025-03-09 06:54:14'),
-(12, NULL, 'ggg@gmail.com', '2025-03-09 16:26:12'),
-(13, NULL, 'hhh@gmail.com', '2025-03-09 16:30:47'),
-(14, NULL, 'jjj@gmail.com', '2025-03-09 17:49:15'),
-(15, NULL, 'kkk@gmail.com', '2025-03-11 04:21:00'),
-(16, NULL, 'lll@gmail.com', '2025-03-11 04:48:30'),
-(17, NULL, 'nnn@gmail.com', '2025-03-11 06:38:58'),
-(18, NULL, 'superadmin@example.com', '2025-03-13 04:14:51'),
-(19, NULL, 'vvv@gmail.com', '2025-03-13 15:33:03'),
-(20, NULL, 'bbb@gmail.com', '2025-03-13 16:48:02'),
-(21, NULL, 'aaa@gmail.com', '2025-03-13 21:01:00'),
-(22, NULL, 'qqqq@gmail.com', '2025-03-14 06:52:14'),
-(23, NULL, 'gd97319741@gmail.com', '2025-03-16 06:04:41'),
-(24, NULL, 'karthik@gmail.com', '2025-03-17 07:41:29'),
-(25, NULL, 'ascscs@gmail.com', '2025-03-18 05:17:59'),
-(26, NULL, 'hello@gmail.com', '2025-03-18 05:28:13');
 
 --
 -- Indexes for dumped tables
